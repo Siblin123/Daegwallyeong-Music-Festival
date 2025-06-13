47217158b12da9d6558ec3d39e2207322beeb3c6
Siblin123 <wlgh7546@naver.com>
2025-06-13 17:40:01 +0900

    scss추가 5시간

--- js/swiper_.js
+++ js/swiper_.js
@@ -8,3 +8,18 @@ var swiper = new Swiper(".poster-Swiper", {
   },
   grabCursor: true, // 👈 커서가 "grab"으로 변하고 드래그 감이 좋아짐
 });
+
+
+
+var swiper = new Swiper(".mainVisual_swiper", {
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
